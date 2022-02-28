025ef1179303805044c78eefc9e093b5a67a43e0
BenSegal855 <ben@segals.org>
2022-02-28 13:50:06 -0500

    feat: all Stevecommands now requrire embed_links

--- src/lib/extensions/SteveCommand.ts
+++ src/lib/extensions/SteveCommand.ts
@@ -1,13 +1,17 @@
 import { Command, CommandOptions, PieceContext } from '@sapphire/framework';
+import { Permissions } from 'discord.js';
 import type { SteveBoi } from './SteveBoi';
 
 export abstract class SteveCommand extends Command {
-
 	public client: SteveBoi;
-	
+
 	public constructor(context: PieceContext, options: CommandOptions) {
-		super(context, options);
+		super(context, {
+			...options,
+			requiredClientPermissions: options.requiredClientPermissions
+				? new Permissions('EMBED_LINKS').add(options.requiredClientPermissions)
+				: 'EMBED_LINKS'
+		});
 		this.client = this.container.client as SteveBoi;
 	}
-
 }
