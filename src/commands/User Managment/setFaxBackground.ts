54a2a1bb4006ee5d3235a1ba9984949da535265c
BenSegal855 <ben@segals.org>
2022-03-05 14:47:44 -0500

    feat: fax color customization commands closes Implement fax colors #16

---
+++ src/commands/User Managment/setFaxBackground.ts
@@ -0,0 +1,35 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import type { Args, CommandOptions } from '@sapphire/framework';
+import { send } from '@sapphire/plugin-editable-commands';
+import { Message, MessageEmbed } from 'discord.js';
+import { SteveCommand } from '../../lib/extensions/SteveCommand';
+
+@ApplyOptions<CommandOptions>({
+	description: 'Set the background color of the faxes you receive.',
+	aliases: ['faxbg', 'faxbackground'],
+	detailedDescription: {
+		usage: '<color>',
+		examples: ['white', '000855']
+	}
+})
+export class UserCommand extends SteveCommand {
+
+	public async messageRun(msg: Message, args: Args) {
+		const color = await args.pick('color');
+
+		await this.client.db.users.findOneAndUpdate(
+			{ id: msg.author.id },
+			{ $set: { 'fax.background': `#${color.toString(16).padStart(6, '0')}` } },
+			{ upsert: true }
+		);
+
+		send(msg, {
+			embeds: [
+				new MessageEmbed()
+					.setColor(color)
+					.setDescription('This is your new fax background color!')
+			]
+		});
+	}
+
+}
