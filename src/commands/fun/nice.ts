1de8f926f638f6c862eac19070bf7942a612fcb2
BenSegal855 <ben@segals.org>
2022-03-08 19:21:30 -0500

    feat: nice 👌 closes #14 Implement nice

---
+++ src/commands/fun/nice.ts
@@ -0,0 +1,15 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import type { CommandOptions } from '@sapphire/framework';
+import type { Message } from 'discord.js';
+import { SteveCommand } from '../../lib/extensions/SteveCommand';
+
+@ApplyOptions<CommandOptions>({
+	description: 'nice'
+})
+export class UserCommand extends SteveCommand {
+
+	public async messageRun(msg: Message) {
+		return msg.react('👌');
+	}
+
+}
