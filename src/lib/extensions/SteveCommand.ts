175818fcbb323724ea77d7f97b9db7585c5b368d
BenSegal855 <ben@segals.org>
2022-03-29 20:24:07 -0400

    feat: Added default subcommand when none present closes #18 Add no-default subcommand handeling

--- src/lib/extensions/SteveCommand.ts
+++ src/lib/extensions/SteveCommand.ts
@@ -1,21 +1,34 @@
-import type { CommandOptions, PieceContext } from '@sapphire/framework';
-import { SubCommandPluginCommand } from '@sapphire/plugin-subcommands';
-import { Permissions } from 'discord.js';
+import type { PieceContext } from '@sapphire/framework';
+import { send } from '@sapphire/plugin-editable-commands';
+import { SubCommandPluginCommand, SubCommandPluginCommandOptions } from '@sapphire/plugin-subcommands';
+import { Message, Permissions } from 'discord.js';
 
 export abstract class SteveCommand extends SubCommandPluginCommand {
 
+	private cmdNames: string[];
 
-	public constructor(context: PieceContext, options: CommandOptions) {
+	public constructor(context: PieceContext, options: SubCommandPluginCommandOptions) {
+		const needsDefault = options.subCommands?.every(subcommand => typeof subcommand === 'string' ? true : !subcommand.default) ?? false;
+		const cmdNames = options?.subCommands?.map(subcommand => typeof subcommand === 'string' ? subcommand : subcommand.input.toString()) ?? [];
+
+		if (needsDefault && options.subCommands) {
+			options.subCommands = [{ input: 'showSubcommands', default: true }, ...options.subCommands];
+		}
+
 		super(context, {
 			...options,
 			requiredClientPermissions: options.requiredClientPermissions
 				? new Permissions('EMBED_LINKS').add(
 					options.requiredClientPermissions
-				  )
+				)
 				: 'EMBED_LINKS'
 		});
+
+		this.cmdNames = cmdNames;
 	}
 
-	// TODO: Implement dynamic default subcommands
+	public showSubcommands(msg: Message) {
+		return send(msg, `You're input is not a valid subcommand. Valid options are \`${this.cmdNames.join('`, `')}\`.`);
+	}
 
 }
