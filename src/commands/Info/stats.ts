fa068848d78fdbbc6d5fbdbdaac8a049375d3c16
BenSegal855 <ben@segals.org>
2022-04-01 20:47:11 -0400

    tweak(stats): botVersion is now attribute

--- src/commands/Info/stats.ts
+++ src/commands/Info/stats.ts
@@ -12,9 +12,10 @@ import { readFileSync } from 'fs';
 })
 export class UserCommand extends SteveCommand {
 
+	private botVersion = JSON.parse(readFileSync(`${process.cwd()}/package.json`).toString()).version;
+
 	public async messageRun(msg: Message) {
 		const { client } = this.container;
-		const { version: botVersion } = JSON.parse(readFileSync(`${process.cwd()}/package.json`).toString());
 
 		send(msg, { embeds: [new MessageEmbed()
 			.setTitle('Statistics')
@@ -27,7 +28,7 @@ export class UserCommand extends SteveCommand {
 				{ name: 'Servers', value: client.guilds.cache.size.toString(), inline: true },
 				{ name: 'Uptime', value: prettyMilliseconds(client.uptime ?? 0, { verbose: true }), inline: true },
 				{ name: 'Memory Usage', value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, inline: true },
-				{ name: `${process.env.BOT_NAME} Version`, value: `v${botVersion}` },
+				{ name: `${process.env.BOT_NAME} Version`, value: `v${this.botVersion}` },
 				{ name: 'Discord.js Version', value: `v${discordVersion}`, inline: true },
 				{ name: 'Sapphire Version', value: `v${sapphireVersion}`, inline: true },
 				{ name: 'Node.js Version', value: process.version, inline: true }
