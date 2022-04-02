fa48d15a448e52a41fcbf8e769fda9b4ffe3f70f
BenSegal855 <ben@segals.org>
2022-04-01 20:40:20 -0400

    fix(stats): get pot version properly

--- src/commands/Info/stats.ts
+++ src/commands/Info/stats.ts
@@ -4,6 +4,7 @@ import { send } from '@sapphire/plugin-editable-commands';
 import { Message, MessageEmbed, version as discordVersion } from 'discord.js';
 import prettyMilliseconds from 'pretty-ms';
 import { SteveCommand } from '@lib/extensions/SteveCommand';
+import { readFileSync } from 'fs';
 
 @ApplyOptions<CommandOptions>({
 	description: `Get some stats about ${process.env.BOT_NAME}.`,
@@ -13,6 +14,8 @@ export class UserCommand extends SteveCommand {
 
 	public async messageRun(msg: Message) {
 		const { client } = this.container;
+		const { version: botVersion } = JSON.parse(readFileSync(`${process.cwd()}/package.json`).toString());
+
 		send(msg, { embeds: [new MessageEmbed()
 			.setTitle('Statistics')
 			.setColor('DARK_GREEN')
@@ -24,7 +27,7 @@ export class UserCommand extends SteveCommand {
 				{ name: 'Servers', value: client.guilds.cache.size.toString(), inline: true },
 				{ name: 'Uptime', value: prettyMilliseconds(client.uptime ?? 0, { verbose: true }), inline: true },
 				{ name: 'Memory Usage', value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, inline: true },
-				{ name: `${process.env.BOT_NAME} Version`, value: `v${process.env.npm_package_version}` },
+				{ name: `${process.env.BOT_NAME} Version`, value: `v${botVersion}` },
 				{ name: 'Discord.js Version', value: `v${discordVersion}`, inline: true },
 				{ name: 'Sapphire Version', value: `v${sapphireVersion}`, inline: true },
 				{ name: 'Node.js Version', value: process.version, inline: true }
