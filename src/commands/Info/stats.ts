0a574da3c2c810beb69ec1b868cd9fd300d1b2d9
BenSegal855 <ben@segals.org>
2022-04-01 20:06:30 -0400

    feat: added stats command closes #22 Add stats commands

---
+++ src/commands/Info/stats.ts
@@ -0,0 +1,36 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { CommandOptions, version as sapphireVersion } from '@sapphire/framework';
+import { send } from '@sapphire/plugin-editable-commands';
+import { Message, MessageEmbed, version as discordVersion } from 'discord.js';
+import prettyMilliseconds from 'pretty-ms';
+import { SteveCommand } from '@lib/extensions/SteveCommand';
+
+@ApplyOptions<CommandOptions>({
+	description: `Get some stats about ${process.env.BOT_NAME}.`,
+	aliases: ['version']
+})
+export class UserCommand extends SteveCommand {
+
+	public async messageRun(msg: Message) {
+		const { client } = this.container;
+		send(msg, { embeds: [new MessageEmbed()
+			.setTitle('Statistics')
+			.setColor('DARK_GREEN')
+			.setThumbnail(client.user?.displayAvatarURL() ?? '')
+			.setTimestamp()
+			.addFields([
+				{ name: 'Users', value: client.users.cache.size.toString(), inline: true },
+				{ name: 'Channels', value: client.channels.cache.size.toString(), inline: true },
+				{ name: 'Servers', value: client.guilds.cache.size.toString(), inline: true },
+				{ name: 'Uptime', value: prettyMilliseconds(client.uptime ?? 0, { verbose: true }), inline: true },
+				{ name: 'Memory Usage', value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, inline: true },
+				{ name: `${process.env.BOT_NAME} Version`, value: `v${process.env.npm_package_version}` },
+				{ name: 'Discord.js Version', value: `v${discordVersion}`, inline: true },
+				{ name: 'Sapphire Version', value: `v${sapphireVersion}`, inline: true },
+				{ name: 'Node.js Version', value: process.version, inline: true }
+
+			])
+		] });
+	}
+
+}
