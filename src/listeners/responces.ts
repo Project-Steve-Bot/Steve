e658a5a9221ea550bc69c11156bcde2fbbe03f7f
BenSegal855 <ben@segals.org>
2022-04-05 20:01:25 -0400

    feat: thanks and love monitors Closes #42 I love you & Thank you

---
+++ src/listeners/responces.ts
@@ -0,0 +1,27 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Events, Listener } from '@sapphire/framework';
+import type { Message } from 'discord.js';
+
+@ApplyOptions<Listener.Options>({
+	event: Events.MessageCreate
+})
+export class UserEvent extends Listener {
+
+	public async run(msg: Message) {
+		if (msg.author.bot) return;
+
+		const lowerContent = msg.content.toLowerCase();
+
+		if (lowerContent.includes((process.env.BOT_NAME ?? 'steve').toLowerCase())) {
+			if (lowerContent.includes('thank')) {
+				msg.react('721911747325460501');
+			}
+		}
+
+		if (lowerContent.includes('love you')) {
+			msg.react('739973420841959549');
+			msg.reply({ content: 'I love you too', allowedMentions: { repliedUser: false } });
+		}
+	}
+
+}
