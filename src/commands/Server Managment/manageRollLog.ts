6ef23c22489c695a98328891e0d558f5e422d43b
BenSegal855 <ben@segals.org>
2022-10-18 19:47:42 -0400

    feat: added roll log

---
+++ src/commands/Server Managment/manageRollLog.ts
@@ -0,0 +1,40 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import type { Args, CommandOptions } from '@sapphire/framework';
+import type { Message } from 'discord.js';
+import { SteveCommand } from '@lib/extensions/SteveCommand';
+import { send } from '@sapphire/plugin-editable-commands';
+
+@ApplyOptions<CommandOptions>({
+	description: 'Sets the channel rolls are logged to.',
+	aliases: ['mrl', 'rollchannel'],
+	requiredUserPermissions: ['MANAGE_CHANNELS'],
+	runIn: ['GUILD_ANY'],
+	detailedDescription: {
+		examples: ['', '#roll_log'],
+		extendedHelp: 'Running this command with no arguments disables the roll log.'
+	}
+})
+export class UserCommand extends SteveCommand {
+
+	public async messageRun(msg: Message, args: Args) {
+		if (!msg.guild) {
+			return send(msg, 'You need to run this command in a server mate.');
+		}
+
+		const channel = args.finished
+			? undefined
+			: await args.pick('guildTextChannel');
+
+		await this.container.db.guilds.updateOne(
+			{ id: msg.guild.id },
+			{ $set: { channels: { rolls: channel?.id } } },
+			{ upsert: true }
+		);
+
+		if (channel) {
+			return send(msg, `Dice rolls will now be logged in <#${channel.id}>.`);
+		}
+		return send(msg, 'Dice rolls are no longer logged.');
+	}
+
+}
