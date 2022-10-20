f607e20223369bce80acf848987a13a043eafe69
BenSegal855 <ben@segals.org>
2022-10-20 16:27:38 -0400

    feat: added the ability to view quick rolls

--- src/commands/User Managment/manageQuickRolls.ts
+++ src/commands/User Managment/manageQuickRolls.ts
@@ -1,7 +1,7 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import type { Args } from '@sapphire/framework';
 import type { SubcommandOptions } from '@sapphire/plugin-subcommands';
-import type { Message } from 'discord.js';
+import { Message, MessageEmbed } from 'discord.js';
 import { SteveSubcommand } from '@lib/extensions/SteveSubcommand';
 import { sendLoadingMessage } from '@lib/utils';
 
@@ -9,11 +9,12 @@ import { sendLoadingMessage } from '@lib/utils';
 	description: 'Add, remove or edit quick rolls.',
 	aliases: ['mqr', 'manageroll', 'managerolls'],
 	detailedDescription: {
-		usage: '<add|remove|edit> <roll name> [roll spec]',
+		usage: '<add|remove|edit|view> <roll name> [roll spec]',
 		examples: [
 			'add qs 1d20+6 d6+4',
 			'edit adv 2d20k1',
-			'remove dart'
+			'remove dart',
+			'view'
 		]
 	},
 	subcommands: [
@@ -22,7 +23,9 @@ import { sendLoadingMessage } from '@lib/utils';
 		{ name: 'remove', messageRun: 'msgRemove' },
 		{ name: 'delete', messageRun: 'msgRemove' },
 		{ name: 'edit', messageRun: 'msgEdit' },
-		{ name: 'update', messageRun: 'msgEdit' }
+		{ name: 'update', messageRun: 'msgEdit' },
+		{ name: 'show', messageRun: 'msgShow' },
+		{ name: 'view', messageRun: 'msgShow' }
 	]
 })
 export class UserCommand extends SteveSubcommand {
@@ -69,6 +72,26 @@ export class UserCommand extends SteveSubcommand {
 		await this.container.db.quickRolls.findOneAndUpdate(quickRoll, { $set: { specs: specs.unwrap() } });
 
 		return response.edit(`**${quickRoll.rollName}** has been updated.`);
+	}
+
+	public async msgShow(msg: Message) {
+		const response = await sendLoadingMessage(msg);
+		const quickRolls = await this.container.db.quickRolls.find({ user: msg.author.id }).toArray();
+
+		if (quickRolls.length === 0) {
+			return response.edit('You currently have no quick rolls');
+		}
+
+		const embed = new MessageEmbed()
+			.setColor('RANDOM')
+			.addFields(quickRolls.map(quickRoll => ({
+				name: quickRoll.rollName,
+				value: quickRoll.specs.map(spec => spec.map(dice => dice.input).join().slice(1)).join(' '),
+				inline: true
+			})));
+
+
+		return response.edit({ embeds: [embed], content: null });
 	}
 
 }
