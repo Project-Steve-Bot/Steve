b5bc1e67a42e5376ea5ac58aaa26dd1e72e11df4
Ben Segal <ben@segals.org>
2025-03-28 21:47:31 -0400

    fix: don't try and use formats if the list is empty

--- src/lib/resolvers/timestamp.ts
+++ src/lib/resolvers/timestamp.ts
@@ -8,7 +8,7 @@ const indicatorRegex = /[ap]m/i;
 export async function resolveTimestamp(parameter: string, user: User): Promise<Result<DateTime, 'TimestampNullMatch' | 'InvalidDateTime' | 'UnknownFormat'>> {
 	const dbUser = await container.db.users.findOne({ id: user.id });
 
-	if (dbUser?.dateTimeFormats) {
+	if (dbUser?.dateTimeFormats && dbUser.dateTimeFormats.length > 0) {
 		for (const format of dbUser.dateTimeFormats) {
 			const potentialTimestamp = DateTime.fromFormat(parameter, format, {
 				zone: dbUser.timezone
